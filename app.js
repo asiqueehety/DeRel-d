75fa50cafdadff6b581e2cc7300455e7cd611683
asiqueehety <asique228@gmail.com>
2024-11-16 23:25:57 +0600

    COMMIT 002: Improved UI and page shifting

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-import express from "express"
+const express = require('express'); // Change import to require
+const path = require('path'); // Change import to require
 
 const app=express()
+
+app.use(express.static(path.join(__dirname, 'public')));
 const port=3000
 
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Define the views directory if it's not in the default location
+app.set('views', path.join(__dirname, 'views')); // Adjust the path if your views are in a different directory
+
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs")
+    res.render("index")
 })
 
 app.listen(port,()=>{
